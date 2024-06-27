434b8c07a5a19ede937a1fdc601082376b57953b
Goran Marjanovic <gm.xsmilex@gmail.com>
2024-06-27 18:46:04 +0200

    algebra hoc added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+const withColor =
+  (Element) =>
+  ({ color }) =>
+    (
+      <Element
+        style={{ backgroundColor: color, height: "20px", width: "100px" }}
+      />
+    );
+
+const Button = (props) => {
+  return <button {...props}></button>;
+};
+
+const ColoredButton = withColor(Button);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ColoredButton color="yellow"></ColoredButton>
+      <ColoredButton color="green"></ColoredButton>
+      <ColoredButton color="blue"></ColoredButton>
     </div>
   );
 }
